8b4624e38313508f3529dc72063b9003e2008172
Luckyaremu <aremu.baba70@gmail.com>
2021-01-23 22:02:15 +0100

    errors fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,28 +15,6 @@ function Footer() {
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year} </h3>
         </Col>
-
-        <Col md="4" className="footer-body">
-          <h6 span className="white">Call or send an Email</h6>
-          <ul className="footer-icons">
-            <li className="social-icons">
-              <a
-                href="#"
-                style={{ color: "white" }}
-              >
-                <i className="fas fa-phone"> +2348032236791</i>
-              </a>
-            </li>
-            <li className="social-icons">
-              <a
-                href="#"
-                style={{ color: "white" }}
-              >
-                <i className="fas fa-envelope-square"> aremu.baba70@gmail.com</i>
-              </a>
-            </li>
-          </ul>
-        </Col>
       </Row>
     </Container>
   );
